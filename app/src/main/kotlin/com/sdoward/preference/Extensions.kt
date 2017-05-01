e80e012b271b0efb8fbc606b3c8b5e17cb7c35d3
samddman <sam.doward@gmail.com>
2017-05-01 19:20:47 +0100

    Add retrieval of all users

--- app/src/main/kotlin/com/sdoward/preference/Extensions.kt
+++ app/src/main/kotlin/com/sdoward/preference/Extensions.kt
@@ -1,6 +1,16 @@
 package com.sdoward.preference
 
+import com.google.firebase.database.DatabaseReference
+
 fun requireBetween(value: Int, lowerValue: Int, higherValue: Int, lazyMessage: () -> Any) {
     require(lowerValue < higherValue) {"lower value is higher or the same as higher value"}
     require(value in lowerValue..higherValue, lazyMessage)
+}
+
+fun DatabaseReference.getUsersReference(): DatabaseReference {
+    return child("users")
+}
+
+fun DatabaseReference.getSessionsReference(): DatabaseReference {
+    return child("sessions")
 }
--- app/src/main/kotlin/com/sdoward/preference/preference/FirebaseRepository.kt
+++ app/src/main/kotlin/com/sdoward/preference/preference/FirebaseRepository.kt
@@ -1,25 +1,42 @@
 package com.sdoward.preference.preference
 
-import android.util.Log
 import com.google.firebase.database.*
+import com.sdoward.preference.getSessionsReference
+import com.sdoward.preference.getUsersReference
 import com.sdoward.preference.signin.User
 import io.reactivex.Single
-
 
 class FirebaseRepository(private val database: DatabaseReference) : Repository {
 
     override fun saveSession(session: Session) {
-        database.child("sessions").push().setValue(session)
+        database.getSessionsReference().push().setValue(session)
     }
 
     override fun saveUser(user: User) {
         val user = mapOf(Pair(user.uid, user))
-        database.child("users").setValue(user)
+        database.getUsersReference().setValue(user)
+    }
+
+    override fun getUsers() : Single<List<User>> {
+        return Single.create { emitter ->
+            database.getUsersReference().addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onCancelled(databaseError: DatabaseError) {
+                    emitter.onError(Throwable(databaseError.message))
+                }
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children
+                            .map { it.getValue(User::class.java) }
+                            .let { emitter.onSuccess(it) }
+                }
+
+            })
+        }
     }
 
     override fun hasUser(uid: String): Single<Boolean> {
         return Single.create<Boolean> {
-            database.child("users").addListenerForSingleValueEvent(object : ValueEventListener {
+            database.getUsersReference().addListenerForSingleValueEvent(object : ValueEventListener {
 
                 override fun onCancelled(databaseError: DatabaseError) {
                     it.onError(Throwable(databaseError.message))
@@ -43,4 +60,6 @@ interface Repository {
     fun saveUser(user: User)
 
     fun hasUser(uid: String): Single<Boolean>
+
+    fun getUsers(): Single<List<User>>
 }
